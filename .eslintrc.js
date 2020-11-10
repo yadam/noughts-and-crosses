970f3c86b8ef9f5c5a1b7ef6ac60d689df6f565d
Adam Bantly <24380+yadam@users.noreply.github.com>
2020-11-09 18:13:42 -0800

    Added some tooling

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'prettier',
+    'prettier/react',
+  ],
+  plugins: ['jest'],
+  env: {
+    browser: true,
+  },
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-curly-newline': 'off',
+    'react/jsx-indent-props': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
